80a9875a6c1df05b7df3a45fa87d7c7026e8773b
Erik6767 <uzakoverik7@gmail.com>
2026-04-02 15:25:19 +0600

    Add number guessing game in main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import random
+
+n = random.randint(1, 100)
+
+print("Угадай число от 1 до 100")
+
+for i in range(3):
+    x = int(input("Число: "))
+    if x > n :
+      print("меньше")
+    if x < n :
+        print("больше")
+    if x == n:
+        print("Угадал")
+        break
+    else:
+        print("Не угадал")
+print("Было:", n)
+
 
